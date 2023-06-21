10e054aab989e2f6d96453f40fc05f6aba0fe6ca
WilliamWJD <william.jose.wjd@gmail.com>
2023-06-21 09:07:40 -0300

    feat: componente input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,35 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
-import BackgroundImg from '@assets/background.png';
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
+import { Input } from "@components/Input";
 
-export function SignIn(){
-    return(
-        <VStack flex={1} bg="gray.700">
-            <Image
-                source={BackgroundImg}
-                alt="Pessoas treinando"
-                resizeMode="contain"
-                position="absolute"
-            />
-        </VStack>
-    )
+export function SignIn() {
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Image
+        source={BackgroundImg}
+        alt="Pessoas treinando"
+        resizeMode="contain"
+        position="absolute"
+      />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e seu corpo
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
+
+        <Input placeholder="E-mail"/>
+        <Input placeholder="Senha"/>
+      </Center>
+    </VStack>
+  );
 }
