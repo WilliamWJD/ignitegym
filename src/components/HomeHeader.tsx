75cf5313cf4e2813fe2bfad479fda79a359450c8
WilliamWJD <william.jose.wjd@gmail.com>
2023-06-29 08:51:30 -0300

    feat: finalizando homeHeader

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,35 @@
+import { TouchableOpacity } from "react-native";
+import { HStack, Heading, Text, VStack, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack
+      bg="gray.600"
+      pt={16}
+      pb={5}
+      px={8}
+      alignItems="center"
+      fontSize="md"
+    >
+      <UserPhoto
+        size={16}
+        source={{ uri: "https://github.com/williamwjd.png" }}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100">Olá</Text>
+        <Heading color="gray.100" fontSize="md">
+          William
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
